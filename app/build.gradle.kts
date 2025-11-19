c29d320d45b1187a38595c0b98335bdc32d0ee46
szymonh <szymon.haponik@cyfroweszkoly.pl>
2025-11-19 12:12:14 +0100

    add more plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,8 @@ dependencies {
 
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
 }
