154d380ec580766b8d10b03111807067c2af5e8d
Szymon <szymon.haponik@cyfroweszkoly.pl>
2025-11-30 18:00:08 +0100

    update view model with updated calsses

--- app/src/main/java/com/example/polskakuchniazadanie/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/example/polskakuchniazadanie/viewmodel/OrderViewModel.kt
@@ -3,13 +3,62 @@ package com.example.polskakuchniazadanie.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.polskakuchniazadanie.model.MealItem
+import com.example.polskakuchniazadanie.model.Order
+import com.example.polskakuchniazadanie.model.PersonOrder
 
 class OrderViewModel: ViewModel() {
-    private val _orderDescription = MutableLiveData<String>()
-    val orderDescription : LiveData<String> get() = _orderDescription
 
-    fun sendOrderDescription (description: String){
-        _orderDescription.value = description
+
+    private var currentPersonOrder = PersonOrder()
+
+    private val allOrders = Order()
+    
+    
+    //-----------------------------------|TOTALBAR LIVE DATA|--------------------------------------------------------------------------------------------
+    private val _currentOrderTotal = MutableLiveData(0.0) // obecna cena zamówienia do total bar
+    val currentOrderTotal: LiveData<Double> get() = _currentOrderTotal
+
+    private val _allOrdersTotal = MutableLiveData(0.0) // całkowita cena zamówienia do total bar
+    val allOrdersTotal: LiveData<Double> get() = _allOrdersTotal
+
+
+    private fun updateCurrentTotal() {
+        _currentOrderTotal.value = currentPersonOrder.getTotal()
     }
+
+    private fun updateAllOrdersTotal() {
+        _allOrdersTotal.value = allOrders.getTotal()
+    }
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    fun addMealToCurrentOrder(item: MealItem) {
+        currentPersonOrder.addItem(item)
+        updateCurrentTotal()
+        
+    }
+
+    fun confirmCurrentOrder() {
+        allOrders.addOrder(currentPersonOrder)
+        updateAllOrdersTotal()
+
+        // zaczynamy następne zamówienie od zera
+        currentPersonOrder = PersonOrder()
+        updateCurrentTotal()
+
+        
+    }
+
+    fun clearAllOrders() {
+        allOrders.personOrders.clear()
+        currentPersonOrder = PersonOrder()
+
+        updateCurrentTotal()
+        updateAllOrdersTotal()
+
+    }
+
+
 
 }
